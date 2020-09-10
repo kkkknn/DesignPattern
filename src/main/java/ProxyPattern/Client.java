a66cf3907e1b7a6ebfb4fb63062a99e92b667916
kkkkkn <1185553557@qq.com>
2020-09-10 18:00:55 +0800

    动态代理模式更新

--- src/main/java/ProxyPattern/Client.java
+++ src/main/java/ProxyPattern/Client.java
@@ -1,5 +1,7 @@
 package ProxyPattern;
 
+import java.lang.reflect.Proxy;
+
 public class Client {
     public static void main(String[] args) {
         /*//构造一个小民————
@@ -20,9 +22,13 @@ public class Client {
         ILawsuit xiaomin=new XiaoMin();
         //构造一个动态代理
         DynamicProxy proxy=new DynamicProxy(xiaomin);
+        //获取被代理类小民的classloader
+        ClassLoader loader=xiaomin.getClass().getClassLoader();
+        //动态构造一个代理者律师
+        ILawsuit lawyer= (ILawsuit) Proxy.newProxyInstance(loader,new Class[]{ILawsuit.class},proxy);
         //律师提交诉讼申请
         lawyer.submit();
-        //律师进行举证
+        //律师进行指证
         lawyer.burden();
         //律师代替小民进行辩护
         lawyer.defend();
